fe44edcb2cd70848e49f57737dd8386c2b6c692d
Alan Orlikoski <alan@orlikoski.com>
2019-01-23 21:17:29 -0600

    build update

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-C:\Python37\Scripts\pyinstaller.exe -F --clean -c "src\cdqr.py" -n cdqr.exe -i "Icons\Martin-Berube-Character-Knight.ico"
+pyinstaller.exe -F --clean -c "src\cdqr.py" -n cdqr.exe -i "Icons\Martin-Berube-Character-Knight.ico"
